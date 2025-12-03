8abef09e1076ab146fe457af5a111b0e6cbc7831
Burlakov Nick <nick@burlakov.io>
2025-12-03 09:26:57 +0100

    day 03

---
+++ day03.py
@@ -0,0 +1,18 @@
+from aoc import *
+
+
+def max_jolts(bank, left):
+    if not bank or left == 0:
+        return ""
+    mx = max(bank[: len(bank) - left + 1])
+    pos = bank.find(mx)
+    return bank[pos] + max_jolts(bank[pos + 1 :], left - 1)
+
+
+def solve(r: Reader):
+    banks = r.read_lines()
+    print(sum(int(max_jolts(bank, 2)) for bank in banks))
+    print(sum(int(max_jolts(bank, 12)) for bank in banks))
+
+
+run(solve)
