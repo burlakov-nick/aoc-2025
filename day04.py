70cbfa126f71387e01823958ae87b1ab297eeaa2
Burlakov Nick <nick@burlakov.io>
2025-12-04 09:39:25 +0100

    day 04 with sets

--- day04.py
+++ day04.py
@@ -2,24 +2,23 @@ from aoc import *
 from helpers import *
 
 
-def can_be_removed(grid, p):
-    return grid[p] == "@" and sum(1 for x in p.neighbors_8() if grid.get(x) == "@") < 4
+def can_be_removed(rolls, p):
+    return sum(1 for x in p.neighbors_8() if x in rolls) < 4
 
 
-def removable(grid):
-    return (p for p in grid.keys() if can_be_removed(grid, p))
+def removable(rolls):
+    return set(p for p in rolls if can_be_removed(rolls, p))
 
 
 def solve(r: Reader):
     grid, n, m = r.read_grid_dict_v()
-    print(count(removable(grid)))
+    initial_rolls = set(p for p, v in grid.items() if v == "@")
+    rolls = set(initial_rolls)
+    print(len(removable(rolls)))
 
-    total = 0
-    while removed := list(removable(grid)):
-        for p in removed:
-            grid[p] = "."
-        total += len(removed)
-    print(total)
+    while len(removed := removable(rolls)):
+        rolls -= removed
+    print(len(initial_rolls) - len(rolls))
 
 
 run(solve)
