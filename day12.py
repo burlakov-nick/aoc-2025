ba0e0a923b6bbc5ac8f6dfe7fdc6040435e9ac80
Burlakov Nick <nick@burlakov.io>
2025-12-12 09:30:42 +0100

    day 12

--- day12.py
+++ day12.py
@@ -1,17 +1,9 @@
 from aoc import *
-from helpers import *
 
 
 def solve(r: Reader):
-    *shapes, tests = r.read_blocks(remove=[":", "x"])
-    shapes = [flatten(shape)[1:] for shape in shapes]
-    shape_areas = [count(flatten(shape), "#") for shape in shapes]
-
-    def is_ok(test):
-        n, m, *counts = test
-        return sum(c * a for c, a in zip(counts, shape_areas)) <= n * m
-
-    print(count(tests, is_ok))
+    *_, tests = r.read_blocks(remove=[":", "x"])
+    print(sum(1 for n, m, *counts in tests if sum(counts) * 7 <= n * m))
 
 
 run(solve, skip_sample=True)
